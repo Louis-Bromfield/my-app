7d487f00595245a5a05d90d7ac67ef5da7b094f2
Louis Bromfield <louisbromwork@gmail.com>
2021-12-04 18:36:17 +0000

    Update server.js
    Removed test comment

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ const path = require("path");
 app.use(express.json());
 app.use(cors());
 
-// Test comment
-
 // Routes
 const homePageNewsFeedRoutes = require('./routes/homePageNewsFeedPosts');
 app.use('/homePageNewsFeedPosts', homePageNewsFeedRoutes);
