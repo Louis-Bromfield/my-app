90c39eefbe45ebc711d92f23c0ebd381b68ad2a6
Louis Bromfield <louisbromwork@gmail.com>
2022-01-17 20:52:59 +0000

    More tweaking.

--- client/src/pages/HomePage/HomeSectionButtons/HomeHeader.js
+++ client/src/pages/HomePage/HomeSectionButtons/HomeHeader.js
@@ -4,11 +4,11 @@ import './HomeHeader.css';
 function HomeHeader(props) {
     return (
         <div className="home-header-intro">
-            <h1>Welcome back, {props.name}! W:{props.width} H:{props.height}</h1>
+            <h1>Welcome back, {props.name}!</h1>
             <p>This is the home page for Fantasy Forecast. Use this as a central hub for 
                 navigating the site! Check out the news feed to see the latest stories that
                 your fellow forecasters have shared, or use any of the shortcuts to visit the 
-                forecasting, learn, and profile pages or the leaderboards!
+                forecasting, learn, and profile pages or the leaderboards! (W:{props.width} H:{props.height})
             </p>
         </div>
     )
