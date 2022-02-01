792d0f3c89acf646d703e8dcdd2e1d6503eaeef6
louisrwb <louisbromwork@gmail.com>
2022-02-01 11:35:33 +0000

    new Date() wrapper

--- client/src/pages/ForecastPage/ForecastComponents/ForecastProblemLineChart.js
+++ client/src/pages/ForecastPage/ForecastComponents/ForecastProblemLineChart.js
@@ -279,7 +279,7 @@ function ForecastProblemLineChart(props) {
                 let increaseY = increaseData[increaseData.length-1].y;
                 let sameY = sameData[sameData.length-1].y;
                 let decreaseY = decreaseData[decreaseData.length-1].y;
-                for (let d = increaseData[increaseData.length-1].x; d <= new Date().toString().slice(0, 15); d.setDate(d.getDate() + 1)) {
+                for (let d = new Date(increaseData[increaseData.length-1].x); d <= new Date().toString().slice(0, 15); d.setDate(d.getDate() + 1)) {
                     let newDate = new Date(d).toString().slice(0, 15);
                     increaseData.push({
                         y: increaseY,
