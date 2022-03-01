d53187166916a948a9345def58cce09e1430c949
Louis Bromfield <louisbromwork@gmail.com>
2022-03-01 13:39:27 +0000

    Had to make a tweak to simulated data.

--- client/src/pages/ForecastPage/ForecastComponents/ForecastProblemLineChart.js
+++ client/src/pages/ForecastPage/ForecastComponents/ForecastProblemLineChart.js
@@ -399,9 +399,9 @@ function ForecastProblemLineChart(props) {
             let increaseY = userIncreaseData[userIncreaseData.length-1].y;
             let sameY = userSameData[userSameData.length-1].y;
             let decreaseY = userDecreaseData[userDecreaseData.length-1].y;
-            let comments = avgIncreaseArr[avgIncreaseArr.length-1].description;
+            let comments = userIncreaseData[userIncreaseData.length-1].description;
             let newDate;
-            if (new Date().toString().slice(0, 15) > selectedForecast.closeDate) {
+            if (new Date().toString().slice(0, 15) < selectedForecast.closeDate) {
                 newDate = new Date().toString().slice(0, 15)
             } else {
                 newDate = selectedForecast.closeDate.slice(0, 15);
