bf34dda1481a172b89a38e2476e8eb034e6059ac
louisrwb <louisbromwork@gmail.com>
2022-06-07 17:01:00 +0100

    Undid shortcut changes

--- client/src/pages/LeaderboardPage/LeaderboardComponents/LeaderboardShortcut.js
+++ client/src/pages/LeaderboardPage/LeaderboardComponents/LeaderboardShortcut.js
@@ -1,24 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './LeaderboardShortcut.css';
 import PlaceholderIcon from '../../../media/leaderboard.png';
-import FFLogo from '../../../media/Icon.png';
-import UnionJackFlag from '../../../media/UnionJackFlag.jpg'
 
 function LeaderboardShortcut(props) {
-    const [leaderboardLogo, setLeaderboardLogo] = useState();
-
-    useEffect(() => {
-        if (props.name === "Fantasy Forecast All-Time") {
-            setLeaderboardLogo(FFLogo);
-        } else if (props.name === "UK Politics") {
-            setLeaderboardLogo(UnionJackFlag);
-        };
-        console.log("LS UE");
-    }, [props.name]);
-
     return (
         <div className={props.className}>
-            <img src={leaderboardLogo} alt="Leaderboard logo" />
+            <img src={PlaceholderIcon} alt=""/>
             <h3>{props.name}</h3>
         </div>
     )
