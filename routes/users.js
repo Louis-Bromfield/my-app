d1b4edad08a262b4b09656bd85314e358ec4f5c0
louisrwb <louisbromwork@gmail.com>
2022-06-19 20:16:10 +0100

    ADded logging

--- routes/users.js
+++ routes/users.js
@@ -184,14 +184,17 @@ router.get("/:username/:passwordOrResetCode/:isPassword", async (req, res) => {
 // Get all scores for an individual forecast problem
 router.get("/getIndividualProblemResults/:problemName", async (req, res) => {
     try {
+        console.log(req.params.problemName);
         // get all users
         const allUsers = await Users.find();
+        console.log(allUsers)
         // make an array of all who have
         let problemRankings = [];
         // loop through all, see if they have a brier for this problem
         for (let i = 0; i < allUsers.length; i++) {
             for (let j = 0; j < allUsers[i].brierScores.length; j++) {
                 if (allUsers[i].brierScores[j].problemName === req.params.problemName) {
+                    console.log("found one!");
                     problemRankings.push({
                         username: allUsers[i].username,
                         score: allUsers[i].brierScores[j].brierScore.toFixed(2)
@@ -201,6 +204,7 @@ router.get("/getIndividualProblemResults/:problemName", async (req, res) => {
         };
         // sort by score
         const sortedProblemRankings = problemRankings.sort((a, b) => b.score - a.score);
+        console.log(sortedProblemRankings);
         // return array
         res.json(sortedProblemRankings);
     } catch (error) {
