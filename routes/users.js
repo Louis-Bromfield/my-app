a6cb2de31dd934a6bdb28fe076dbe0ece0f1e5d1
louisrwb <louisbromwork@gmail.com>
2023-02-01 15:47:25 +0000

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -754,7 +754,7 @@ console.log(teamDocument.members.length);
                 username: req.body.teamName,
                 members: [req.params.username],
                 profilePicture: "https://lh3.googleusercontent.com/a/AItbvmkRUSgd_Izrhz4X-ft3do7Li1X0OsBPAzgh9r4G=s96-c",
-                isTeam: false
+                isTeam: true
             });
             await newTeamDocument.save();
             message = "Team successfully created";
